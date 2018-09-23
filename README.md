e9a13bf825dcf39e822fd77fa9026a21b29916f1
LeonardoP <hlpenam@gmail.com>
2018-09-23 02:41:47 -0500

    Proyecto Actualizado
    Se realizan los cambios de los formularios, de la fecha 22 sep 2018

--- README.md
+++
@@ -1,2 +0,0 @@
-# proyecto
-Proyecto TImiza Park Lot
--- ingreseotra.php
+++
@@ -1,6 +0,0 @@
-<?php echo "saliste de sesion ingresa a login de nuevo";
-session_start();
-session_destroy();
-echo "<script>window.location='index.php'</script>";
-
- ?>
